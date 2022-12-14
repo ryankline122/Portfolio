f681ddd34f8c73ab2b79e48d4580b722f26bd674
Ryan Kline <ryankline4780@gmail.com>
2022-12-13 19:12:02 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Intro from './components/Intro';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Intro />
+      <Portfolio />
+      <Contact />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Portfolio(){
+    return (
+        <div>My Projects</div>
+    )
+}
+
+export default Portfolio;

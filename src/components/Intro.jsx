13df4399a9f1493fcf081d4ede5a983f197ffc69
Ryan Kline <ryankline4780@gmail.com>
2023-01-08 15:05:42 -0500

    Update intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -7,7 +7,7 @@ function Intro(){
             <div className="border-b-2 mb-10">
                 <h1 className="text-4xl md:text-6xl mb-1 md:mb-5 font-bold">Ryan Kline</h1>
                 <h4 className="text-xl mb-1 md:mb-3">4th Year Computer Science Student at Grand Valley State University</h4>
-                <h5 className="text-xl mb-1 md:mb-10">Seeking Internship for Summer 2023</h5>
+                <h5 className="text-xl mb-1 md:mb-10">Incoming Application Developer Intern at Corewell Health</h5>
             </div>
             
 
@@ -19,17 +19,15 @@ function Intro(){
                 
                 In addition to my studies, I have also had the opportunity to intern as a software engineer at 
                 DISHER. During my internship, I worked on a number of projects, including 
-                writing software tests for an autonomous rover, an internal marketing app, and a device to make the electronics lab more
-                accessible to remote team members. 
-                This experience allowed me to apply my knowledge and skills in a professional setting, and further develop 
+                writing software tests for an autonomous rover, an inventory system for our lab, and a device to make hands-on collaboration more
+                accessible to remote team members. This experience allowed me to apply my knowledge and skills in a professional setting, and further develop 
                 my abilities as a programmer. As I complete my degree, I am eager to continue learning and growing as 
                 a software engineer. I am confident in my abilities and am excited to begin my career in the field.
                 
                 <br></br><br></br>
 
-                I am currently open to job opportunities, so if you are looking for a skilled and dedicated software engineer, 
-                please don't hesitate to contact me. 
-                I would love the opportunity to discuss how I can contribute to your team!
+                I am not seeking any new opportunities at the moment, but you are more than welcome to connect with me on <a class="underline" href="https://www.linkedin.com/in/ryanjkline/">LinkedIn</a> should 
+                anything come up in the future!
                 </p>
             </div>
         </div>
--- src/data/timeline.js
+++ src/data/timeline.js
@@ -4,7 +4,7 @@ export default [
         year: 'Current',
         title: 'Software Engineering Intern',
         company: 'DISHER',
-        duration: '7 months',
+        duration: '8 months',
         location: 'Zeeland, MI',
         details: 
         ' As a software engineering intern at DISHER, I have had the opportunity to work on a range of exciting projects that have helped me develop my technical skills and gain valuable experience in the field. For example, I collaborated with a team of engineering interns to design and build a device that improved remote access to our electronics lab. I also developed, updated, and maintained unit and system tests on an autonomous rover. Additionally, I created an interactive marketing application that was displayed around the office to showcase DISHER\'s culture to prospective clients and new hires. Currently, I am working on developing an inventory system using the Django web framework. Through these projects, I have gained valuable experience in software development, testing, and collaboration, and have demonstrated my ability to deliver high-quality results in a fast-paced, team-oriented environment.'
